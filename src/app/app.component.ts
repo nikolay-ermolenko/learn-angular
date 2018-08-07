89404708375b465a6cd4244cda8b6886a85c1615
nikolay <forum@terni.ru>
2018-08-07 22:54:15 +0300

    test page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+
+
+    @HostBinding('style.fontSize.px')
+    private _fontSize: number = 17;
+
+    title = 'angular';
+
+    public ngOnInit(): void {
+
+    }
+
+    public get fontSize(): number {
+        return this._fontSize;
+    }
+    public set fontSize(value: number) {
+        this._fontSize = value;
+    }
 }
